b5262445a452fd7b32e7329f2f6f678ff8315410
Pariwesh Gupta <pariwesh.gupta@publicissapient.com>
2023-02-20 10:21:44 +0530

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <UserForm/>
+  <input/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import UserForm from './components/UserForm.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    UserForm
   }
 }
 </script>
---
+++ src/components/UserForm.vue
@@ -0,0 +1,21 @@
+<template>
+    <h4>User form </h4>
+    <input />
+</template>
+
+<script>
+
+export default {
+  name: 'UserForm',
+  components: {
+    
+  }
+}
+</script>
+
+
+<style>
+    input {
+        background-color:yellow
+    }
+</style>
